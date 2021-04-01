c9fae883dbef05d3b3ccfda312850f17b3112483
abanoub-fahmy <abanoub99999@gmail.com>
2021-04-01 23:48:53 +0200

    LED 1.0.0

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * LCD.c
+ *
+ * Created: 3/26/2021 1:09:17 PM
+ * Author : Infotech
+ */ 
+
+#include "LCD.h"
+
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    LCD_Intit();
+    LCD_GoToLocation(0,6);
+	LCD_WriteInteger(1255);
+
+
+	
+	
+	
+	
+}
+
